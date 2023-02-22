cd5e1a0b85a5bee2cda5c56d9647ed7363f6e4da
Vansh Wassan <007blackhacker@gmail.com>
2023-02-22 11:36:51 +0530

    added licences to the token contracts

--- contracts/WETH.sol
+++ contracts/WETH.sol
@@ -1,4 +1,9 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.9;
+
+/// @title Basic openzeppelin ERC20 Token.
+/// @author @vanshwassan
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
