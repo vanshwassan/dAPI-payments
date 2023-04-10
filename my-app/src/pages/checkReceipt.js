d50880135b3581b41810754684825188c1af48cc
Vansh Wassan <007blackhacker@gmail.com>
2023-04-10 19:57:34 +0530

    [WIP] Added checkReceipt functionality.

--- my-app/src/pages/checkReceipt.js
+++ my-app/src/pages/checkReceipt.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react'
 import {
     Flex,
     Box,
@@ -10,7 +10,43 @@ import {
     Button,
 } from '@chakra-ui/react';
 
-export default function Check() {
+import { ethers } from 'ethers';
+
+
+export default function Check(props) {
+    const [amount, setAmount] = useState(null);
+    const [provider, setProvider] = useState(null);
+    const [signer, setSigner] = useState(null);
+    const [contract, setContract] = useState(null);
+    const ABI = require('./abi/Payments.json');
+    const PaymentsContractAddress = "0x1D982184951f7A1C819a1eD6879C14af0Aa83cD7";
+
+    const checkReceipt = async (e) => {
+            e.preventDefault();
+            console.log("Checking Receipt");
+            let tempProvider = new ethers.providers.Web3Provider(window.ethereum);
+            setProvider(tempProvider);
+    
+            const signer = tempProvider.getSigner();
+            setSigner(signer);
+            console.log("Signer set successfully")
+        
+            let contract = new ethers.Contract(PaymentsContractAddress, ABI, signer);
+            setContract(contract);
+            console.log("Contract Loaded Successfully")
+
+            const tokenID = e.target.tokenid.value;
+            console.log(tokenID)
+            try {
+            const check = await contract.checkReceipt(tokenID, { gasLimit: 1000000 });
+            console.log(check.toString());
+            const finalAmount = check.toString()/10**18;
+            setAmount(finalAmount);
+            } catch (err) {
+            console.error(err);
+            }
+        };
+
     return (
         <Flex width="full" align="center" justifyContent="center">
             <Box p={2}>
@@ -19,14 +55,15 @@ export default function Check() {
                     <Heading size={2}>Check how much you paid to the contract in USD</Heading>
                 </Box>
                 <Box my={4} textAlign="left">
-          <form>
+          <form onSubmit={checkReceipt}>
             <FormControl mt={6}>
               <FormLabel>Enter the TokenID</FormLabel>
-              <Input type="text" />
+              <Input type="text" id='tokenid'/>
             </FormControl>
             <Button width="full" mt={4} type="submit">
               Check
             </Button>
+            <p>You Paid : ${ amount }</p>
           </form>
         </Box>
             </Box>
